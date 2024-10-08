6968e487f9c28aabb1449bdbb27f8c15ede04f33
Rafael Rosa <79066006+rarorza@users.noreply.github.com>
2024-10-08 19:02:39 -0300

    Update community.lua

--- astronvim/lua/community.lua
+++ astronvim/lua/community.lua
@@ -17,11 +17,11 @@ return {
   { import = "astrocommunity.pack.godot" },
   { import = "astrocommunity.code-runner.overseer-nvim" },
   { import = "astrocommunity.git.diffview-nvim" },
-  { import = "astrocommunity.colorscheme.monokai-pro-nvim" },
+  { import = "astrocommunity.colorscheme.dracula-nvim" },
   { import = "astrocommunity.color.transparent-nvim" },
   { import = "astrocommunity.editing-support.vim-visual-multi" },
   { import = "astrocommunity.editing-support.vim-move" },
-  { import = "astrocommunity.programming-language-support.web-tools-nvim" },
-  { import = "astrocommunity.game.leetcode-nvim" },
+  { import = "astrocommunity.search.grug-far-nvim" }, -- use \b  \b to replace exactly same thing
+  { import = "astrocommunity.programming-language-support
   -- import/override with your plugins folder
 }
